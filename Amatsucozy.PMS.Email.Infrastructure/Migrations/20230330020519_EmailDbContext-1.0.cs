7f3a3349f76dd3c88ffa93b4ef7f6958d24559cc
Amatsucozy <amatsucozy@gmail.com>
2023-03-30 09:13:10 +0700

    Email - Add Migrations

---
+++ Amatsucozy.PMS.Email.Infrastructure/Migrations/20230330020519_EmailDbContext-1.0.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Amatsucozy.PMS.Email.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailDbContext10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "email");
+
+            migrationBuilder.CreateTable(
+                name: "EmailTemplate",
+                schema: "email",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    Placeholders = table.Column<string>(type: "text", nullable: false),
+                    EnableMultipleReceivers = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailTemplate", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailTemplate",
+                schema: "email");
+        }
+    }
+}
